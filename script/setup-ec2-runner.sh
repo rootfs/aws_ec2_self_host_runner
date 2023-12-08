e1a99fe291d95c360e97374eff80040147783d3b
Huamin Chen <hchen@redhat.com>
2023-12-07 21:28:21 -0500

    update
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- script/setup-ec2-runner.sh
+++ script/setup-ec2-runner.sh
@@ -25,7 +25,7 @@
 set -o pipefail
 
 # Define instance parameters
-AMI_ID="${AMI_ID:-ami-0e83be366243f524a}" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type, x86_64
+AMI_ID="${AMI_ID:-ami-0e4d0bb9670ea8db0}" # Ubuntu Server 20.04 LTS (HVM), SSD Volume Type, x86_64
 INSTANCE_TYPE="${INSTANCE_TYPE:-t2.micro}" # c6i.metal: c is for compute, 6 is 6th geneneration, i is for Intel, metal is for bare metal
 SECURITY_GROUP_ID="${SECURITY_GROUP_ID:-YOUR_SECURITY_GROUP_ID}"
 GITHUB_TOKEN="${GITHUB_TOKEN:-YOUR_TOKEN}"
